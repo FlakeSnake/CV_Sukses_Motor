b81779d0402eade3bb6bcecf3296b3269a9e7908
AndouJuraii <72550116+AndouJuraii@users.noreply.github.com>
2022-03-24 16:40:35 +0700

    Update home.blade.php
    Testing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Jancok   ') }}
                 </div>
             </div>
         </div>
