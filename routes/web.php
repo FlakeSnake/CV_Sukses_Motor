9a2f6a500a571e1a60c0c568779bdaa1e3c1a8f9
Jericho <jerichohuang26@gmail.com>
2022-03-24 13:30:17 +0700

    tes

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // return view('auth.login');
+
+});
+
+Route::get('/halo', function () {
+    return "Halo Ahok";
+});
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'login'])->name('login');
