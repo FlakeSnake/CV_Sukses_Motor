c57f6d438afd770ede3b63b6b933cdcc475c84e1
AndouJuraii <72550116+AndouJuraii@users.noreply.github.com>
2022-06-07 02:06:55 +0700

    Fix security
    Fix ya Fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -283,6 +283,30 @@
         });
     </script>
 
+        <script>
+            document.onkeypress = function (event) {
+            event = (event || window.event);
+            if (event.keyCode == 123) {
+            return false;
+            }
+            }
+            document.onmousedown = function (event) {
+            event = (event || window.event);
+            if (event.keyCode == 123) {
+            return false;
+            }
+            }
+            document.onkeydown = function (event) {
+            event = (event || window.event);
+            if (event.keyCode == 123) {
+            return false;
+            }
+            }
+        </script>
+
+
+
+
 
 
 
